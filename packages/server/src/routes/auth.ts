e1b5fee684c93f696dbcc491dd51fa59f85bce56
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-05-29 19:16:22 -0400

    chore: add auth

--- packages/server/src/routes/auth.ts
+++ packages/server/src/routes/auth.ts
@@ -1,33 +1,54 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { createUser } from "../db/drizzle/orm/users/user";
+import { supabaseClient } from "../db/supabase/spabaseClient";
 
 export const auth = new Elysia({ prefix: "/auth" })
   .get("/", () => "Hello Elysia")
-  .post("/login", async (req) => {
-    console.log("creating user", req.body);
+  .guard({
+    body: t.Object({
+      password: t.String(),
+      name: t.String(),
+      email: t.String(),
+    })
+  }, (app) => app.post("/signup", async (req) => {
 
-    const user = await createUser("req.body?.name", "req.body?.email" as string);
+    const { data, error } = await supabaseClient.auth.signUp({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (error || !data.user?.id) throw new Error("/signup: " + error ? error?.message : "no data");
+
+    const [user] = await createUser(req.body.name, req.body?.email, data.user?.id);
     console.log("user created", user);
-
     return {
       user: {
-        name: "Elysia",
-        email: "<EMAIL>",
-        password: "<PASSWORD>",
+        name: user.name,
+        email: user.email,
+        id: user.id,
       },
     };
   })
-  .post("/signup", async (req) => {
-    console.log("creating user", req.body);
-    // @ts-ignore
-    const user = await createUser(req.body?.name, req.body.email as string);
-    console.log("user created", user);
+    .post("/login", async (req) => {
+      console.log("login", req.body);
 
-    return {
-      user: {
-        name: "Elysia",
-        email: "<EMAIL>",
-        password: "<PASSWORD>",
-      },
-    };
-  })
+      const { data, error } = await supabaseClient.auth.signInWithPassword({
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      if (error) throw new Error("/login:" + error.message);
+
+      // find the user in the DB
+
+      // const user = await createUser(req.body?.name, req.body.email as string);
+      // get on the user DB 
+      console.log("user logged", data);
+
+      return {
+        user: {
+          name: data.user.id,
+          email: data.user.email,
+        },
+      };
+    }))
